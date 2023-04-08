8391d469b7edda041fad90178ab6de875c5900c4
Ahmed-Elakeed <ahmed.elaked20@gmail.com>
2023-04-08 22:03:33 +0200

    Edited services

---
+++ src/main/java/com/bfcai/ECH/service/CompanionService.java
@@ -0,0 +1,138 @@
+package com.bfcai.ECH.service;
+
+import com.bfcai.ECH.dao.CompanionRepository;
+import com.bfcai.ECH.dao.PatientRepository;
+import com.bfcai.ECH.dto.ApiResponseDto;
+import com.bfcai.ECH.dto.ResponseData;
+import com.bfcai.ECH.entity.Companion;
+import com.bfcai.ECH.entity.Patient;
+import com.bfcai.ECH.type.StatusCode;
+import com.bfcai.ECH.type.StatusMessage;
+import com.bfcai.ECH.wrapper.CompanionWrapper;
+import com.bfcai.ECH.wrapper.PatientWrapper;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CompanionService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final CompanionRepository companionRepository;
+    private final ModelMapper modelMapper;
+
+    public ApiResponseDto saveCompanion(Companion companion) {
+        ApiResponseDto responseDto = new ApiResponseDto<>();
+        if (this.validateCompanionData(companion)) {
+            responseDto = ApiResponseDto.builder()
+                    .responseData(ResponseData.builder()
+                            .data(this.companionRepository.save(companion))
+                            .count(1L)
+                            .build())
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .message(StatusMessage.CREATED)
+                    .build();
+        } else {
+            responseDto.setCode(StatusCode.ALREADY_EXISTS.serverCode);
+            responseDto.setMessage(StatusMessage.ALREADY_EXISTS);
+        }
+        return responseDto;
+    }
+
+    @Transactional
+    public void savePatientCompanion(Companion companion) {
+        for (Patient patient: companion.getPatients()) {
+            this.entityManager.createNativeQuery("insert into patient_companion (patient_id,companion_id) values (?,?)")
+                    .setParameter(1, patient.getId())
+                    .setParameter(2, companion.getId())
+                    .executeUpdate();
+        }
+    }
+
+
+    private boolean validateCompanionData(Companion companion) {
+        List<Companion> companions = this.companionRepository.findCompanionByEmailOrPhone(companion.getEmail(), companion.getPhone());
+        return companions.isEmpty();
+    }
+
+    public ApiResponseDto getSpecificCompanion(Long companionId) {
+        ApiResponseDto responseDto = new ApiResponseDto<>();
+        CompanionWrapper companion = modelMapper.map(this.companionRepository.findById(companionId), CompanionWrapper.class);
+        if (companion != null) {
+            responseDto = ApiResponseDto.builder()
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .message(StatusMessage.SUCCESS)
+                    .responseData(ResponseData.builder()
+                            .data(companion)
+                            .count(1L)
+                            .build())
+                    .build();
+        } else {
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+        }
+        return responseDto;
+
+    }
+
+    //Getting all patients for companion
+    public ApiResponseDto getAllPatientsForCompanion(Long companionId) {
+        ApiResponseDto responseDto = new ApiResponseDto<>();
+        boolean isExist = this.companionExist(companionId);
+        if (isExist) {
+            List<PatientWrapper> patients = this.companionRepository.findAllPatientsForCompanion(companionId)
+                    .stream().map(patient -> this.modelMapper.map(patient, PatientWrapper.class))
+                    .collect(Collectors.toList());
+            responseDto = ApiResponseDto.builder()
+                    .responseData(ResponseData.builder()
+                            .data(patients)
+                            .count((long) patients.size())
+                            .build())
+                    .message(StatusMessage.SUCCESS)
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .build();
+        } else {
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+        }
+        return responseDto;
+    }
+
+    //Check companion exist
+    public boolean companionExist(Long companionId) {
+        Companion companion = this.companionRepository.findById(companionId).orElse(null);
+        if (companion != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Delete Companion
+    public ApiResponseDto deleteCompanion(Long companionId) {
+        ApiResponseDto responseDto = new ApiResponseDto<>();
+        if (this.companionExist(companionId)) {
+            this.companionRepository.deleteById(companionId);
+            responseDto = ApiResponseDto.builder()
+                    .responseData(null)
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .message(StatusMessage.SUCCESS)
+                    .build();
+        } else {
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+        }
+        return responseDto;
+    }
+}
---
+++ src/main/java/com/bfcai/ECH/service/MedicineService.java
@@ -0,0 +1,94 @@
+package com.bfcai.ECH.service;
+
+
+import com.bfcai.ECH.controller.MedicineController;
+import com.bfcai.ECH.dao.MedicineRepository;
+import com.bfcai.ECH.dto.ApiResponseDto;
+import com.bfcai.ECH.dto.ResponseData;
+import com.bfcai.ECH.entity.Medicine;
+import com.bfcai.ECH.type.StatusCode;
+import com.bfcai.ECH.type.StatusMessage;
+import com.bfcai.ECH.wrapper.MedicineWrapper;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MedicineService {
+
+    private final MedicineRepository medicineRepository;
+    private final ModelMapper modelMapper;
+
+    public ApiResponseDto saveMedicine(Medicine medicine){
+        ApiResponseDto responseDto=new ApiResponseDto<>();
+        responseDto=ApiResponseDto.builder()
+                    .responseData(ResponseData.builder()
+                            .data(this.medicineRepository.save(medicine))
+                            .count(1L)
+                            .build())
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .message(StatusMessage.SUCCESS)
+                    .build();
+        return responseDto;
+    }
+
+
+    //Getting medicine by patient id
+    public ApiResponseDto findMedicineByPatientId(Long patientId){
+        ApiResponseDto responseDto=new ApiResponseDto<>();
+        List<MedicineWrapper> medicine=this.medicineRepository.findAllByPatientId(patientId)
+                .stream()
+                .map(medicine1 -> this.modelMapper.map(medicine1,MedicineWrapper.class))
+                .collect(Collectors.toList());
+        if(medicine!=null){
+            responseDto=ApiResponseDto.builder()
+                    .responseData(ResponseData.builder()
+                            .data(medicine)
+                            .count((long) medicine.size())
+                            .build())
+                    .code(StatusCode.SUCCESS.serverCode)
+                    .message(StatusMessage.SUCCESS)
+                    .build();
+        }else {
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+        }
+        return responseDto;
+    }
+
+    //Delete By Id
+    public ApiResponseDto deleteByMedicineId(Long id){
+        ApiResponseDto responseDto=new ApiResponseDto<>();
+        Medicine medicine=this.medicineRepository.findById(id).orElse(null);
+        if(medicine!=null){
+            this.medicineRepository.deleteById(id);
+            responseDto.setMessage(StatusMessage.SUCCESS);
+            responseDto.setCode(StatusCode.SUCCESS.serverCode);
+        }else {
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+        }
+        return responseDto;
+    }
+
+
+    //Delete By patient Id
+    public ApiResponseDto deleteByPatientId(Long patientId){
+        ApiResponseDto responseDto=findMedicineByPatientId(patientId);
+        if(responseDto.getResponseData()!=null){
+            List<Medicine> medicines= this.medicineRepository.findAllByPatientId(patientId);
+            for (Medicine m : medicines){
+                this.medicineRepository.deleteById(m.getId());
+            }
+        }
+        else {
+            responseDto.setMessage(StatusMessage.NOT_FOUND);
+            responseDto.setCode(StatusCode.NOT_FOUND.serverCode);
+        }
+        return responseDto;
+    }
+}
