13fec3fad9906ea2a83eea0f259161302efd4126
Ahmed-Elakeed <ahmed.elaked20@gmail.com>
2023-05-07 12:57:27 +0300

    symptom checker service

---
+++ src/main/java/com/bfcai/ECH/service/DiseasePredictionService.java
@@ -0,0 +1,48 @@
+package com.bfcai.ECH.service;
+
+import com.bfcai.ECH.dao.DiseaseRepository;
+import com.bfcai.ECH.dto.DiseasePredictionResponse;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+
+@Service
+@RequiredArgsConstructor
+public class DiseasePredictionService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final DiseaseRepository diseaseRepository;
+
+    @SuppressWarnings("unchecked")
+    public List<DiseasePredictionResponse> fetchExpectedDiseases(List<String> symptoms) {
+
+        List<String> diseases = (List<String>) this.entityManager.createNativeQuery("select d.disease from diseases d join" +
+                        " (select disease_id from disease_symptoms where symptom_id in" +
+                        " (select s.id from symptoms s where s.symptom in (?))) dj" +
+                        " on d.id=dj.disease_id")
+                .setParameter(1, symptoms)
+                .getResultList();
+
+        Set<String> notDuplicatedDiseases = new HashSet<>(diseases);
+
+        List<DiseasePredictionResponse> diseasePredictionResponses = new ArrayList<>();
+
+        notDuplicatedDiseases.forEach(value -> diseasePredictionResponses.add(
+                new DiseasePredictionResponse(
+                        ((float) diseases.stream().filter(disease -> disease.equals(value)).count() / (float) diseases.size() * (float) 100.0),
+                        value,
+                        this.diseaseRepository.fetchDescriptionForDisease(value)
+
+                )
+        ));
+
+        diseasePredictionResponses.sort((o1, o2) -> (int) (o2.getProbability() - o1.getProbability()));
+
+        return diseasePredictionResponses;
+    }
+}
