d79eb1a23acd0c10d65043b27a2546c9bff2bbe5
Ahmed-Elakeed <ahmed.elaked20@gmail.com>
2023-05-18 19:38:51 +0300

    Symptom checker update

--- src/main/java/com/bfcai/ECH/service/DiseasePredictionService.java
+++ src/main/java/com/bfcai/ECH/service/DiseasePredictionService.java
@@ -44,8 +44,9 @@ public class DiseasePredictionService {
 
         diseasePredictionResponses.sort((o1, o2) -> (int) (o2.getProbability() - o1.getProbability()));
         List<DiseasePredictionResponse> result=new ArrayList<>();
-        diseasePredictionResponses.get(0).setProbability(70);
-        diseasePredictionResponses.get(1).setProbability(20);
+        float sum=diseasePredictionResponses.get(0).getProbability()+diseasePredictionResponses.get(1).getProbability();
+        diseasePredictionResponses.get(0).setProbability(((diseasePredictionResponses.get(0).getProbability())/sum)*100);
+        diseasePredictionResponses.get(1).setProbability(((diseasePredictionResponses.get(1).getProbability())/sum)*100);
         result.add(diseasePredictionResponses.get(0));
         result.add(diseasePredictionResponses.get(1));
         return result;
